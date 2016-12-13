02413f91b1f4575d4e5be386e2d5964b6c690028
Igor Kamyshev <ikamyshev@exchange.starpoint.com>
2016-12-13 12:59:25 +0200

    NLog: catch application errors

--- ASPNETSimple.WEB/Global.asax.cs
+++ ASPNETSimple.WEB/Global.asax.cs
@@ -9,10 +9,36 @@ namespace ASPNETSimple
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
         protected void Application_Start()
         {
+            logger.Info("Application Start");
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        public override void Init()
+        {
+            logger.Info("Application Init");
+            base.Init();
+        }
+
+        public override void Dispose()
+        {
+            logger.Info("Application Dispose");
+            base.Dispose();
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception exception = Server.GetLastError();
+            logger.Error(exception, "Application Error");
+        }
+
+
+        protected void Application_End()
+        {
+            logger.Info("Application End");
         }
     }
 }
