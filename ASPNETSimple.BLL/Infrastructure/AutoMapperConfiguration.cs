bda747f8565b88219c02e34e9c13a42670f1318c
Igor Kamyshev <ikamyshev@exchange.starpoint.com>
2016-12-14 18:25:16 +0200

    BLL: AutoMapper updates

--- ASPNETSimple.BLL/Infrastructure/AutoMapperConfiguration.cs
+++ ASPNETSimple.BLL/Infrastructure/AutoMapperConfiguration.cs
@@ -1,16 +1,21 @@
-﻿using ASPNETSimple.BLL.Infrastructure.Mappings;
+﻿using System;
+using ASPNETSimple.BLL.Infrastructure.Mappings;
 using AutoMapper;
 
 namespace ASPNETSimple.BLL.Infrastructure
 {
+
     public class AutoMapperConfiguration
     {
+        public static Action<IMapperConfigurationExpression> ProjectMappings = new Action<IMapperConfigurationExpression>(x =>
+        {
+            x.AddProfile<ModelsToEntitiesProfile>();
+        });
+
         public static void Configure()
         {
-            Mapper.Initialize(x =>
-            {
-                x.AddProfile<ModelsToEntitiesProfile>();
-            });
+            Mapper.Initialize(ProjectMappings);
         }
+        
     }
 }
