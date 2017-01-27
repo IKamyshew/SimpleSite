4c85708ea3f43175288f05f5624cb0f2bd198167
Igor Kamyshev <ikamyshev@exchange.starpoint.com>
2017-01-27 14:22:22 +0200

    WEB.Tests: Adding packages Ninject,Moq,Castle.Core

---
+++ ASPNETSimple.WEB.Tests/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using ASPNETSimple.BLL.Services;
+using ASPNETSimple.BLL.Services.Interfaces;
+using Ninject;
+
+namespace ASPNETSimple.WEB.Tests.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+        private void AddBindings()
+        {
+            kernel.Bind<IUserService>().To<UserService>();
+        }
+    }
+}
